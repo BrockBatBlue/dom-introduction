ed776ad7128b1f81bdb6fee9cb558711a07b5238
Tekalegn Fisseha <objectp@yahoo.com>
2022-06-17 08:57:39 -0400

    TeacherDemoDOM2 fiels modified

--- TeacherDemoDOM2/script.js
+++ TeacherDemoDOM2/script.js
@@ -1,16 +1,10 @@
-// Let's look at the HTML page and see what elements we can GRAB using .querySelectorAll()
-let coolDivs = document.querySelectorAll("div");
-let paraG = document.querySelectorAll("p");
-let imgYes = document.querySelectorAll("img");
+let body = document.body;
+let mianTitle = document.getElementById('main-title')
+let subTitle = document.querySelector("h2");// returns the first element
+let firstParagraph = document.querySelector('#first-paragraph')
+let allDivs = document.querySelectorAll('div') // returns an array of divs 
+let crazyChange = document.getElementsByClassName('crazy-change') // array
+let slothImage = document.querySelectorAll("img"); //returns an array 
+let clickMeButton = document.getElementById("click-me-button")
 
-// Instead of getElementById, you can also use a querySelector to grab an element by it's Id
-let changeMe = document.querySelector("#changeMeById");
 
-// We can also grab elements by their class names
-let bossChange = document.getElementsByClassName("boss-div");
-
-// Now that we have grabbed these elements, let's make some changes!
-// I want an actual image!
-imgYes[0].setAttribute("src", "./public/images/playfulSloth.jpg");
-// Beautiful! Now let's make it a little smaller, maybe?
-imgYes[0].setAttribute("style", "width:25%");
